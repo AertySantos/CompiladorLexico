1a45840a9c28071f02009a8afaa5c65a3bf127d4
AERTY  PINTO DOS SANTOS <93597919+AertySantos@users.noreply.github.com>
2022-05-22 22:12:53 +0000

    teste jj

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <stdio.h>
 #include "preCompilador.h"
+
 using namespace std;
 
 int main() {
   
+  /*string t = "__USE_LARGEFILE64";
+  if(t == "__USE_LARGEFILE64"){
+    if(__USE_LARGEFILE64){
+      cout<<"incluir esse trecho de codigo";
+    }
+  }*/
+  
+  
   PreCompilador pc;
   pc.leitura();
-  
-  
+
 }

5abdf139f2cfc9a87c3c57cd3b569b0d13626f03
AERTY  PINTO DOS SANTOS <93597919+AertySantos@users.noreply.github.com>
2022-05-31 23:53:35 +0000

    main atualizado

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include "preCompilador.h"
 #include "leitura.h"
-#include "GeradorToken.h"
+//#include "GeradorToken.h"
 
 using namespace std;
 
 int main() {
   
-  PreCompilador pc;
-  pc.iniciar();
+  //PreCompilador pc;
+  //pc.iniciar();
+  
+  Leitor l;
+  list<string> lista = l.ler("exemplos/exemplo2.c");
+  cout << "SAIDA:"<<endl;
+  for(string s: lista) {
+    cout<< "<" << s << "> ";
+  }
 
 }

791da9bcc7e0db58f195c4175852a3c7431cf3bc
jacques3d <45763539+jacques3d@users.noreply.github.com>
2022-06-05 22:21:24 +0000

    HashTable

---
+++ HashTable.h
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <list>
+#include <cstring>
+
+using namespace std;
+
+class HashTable
+{
+private:
+    static const int hashGroups = 10;
+    list<pair<int, string>> tabela[hashGroups]; // list 1, indice 0, list 2, indice 1...
+public:
+    bool estaVazia() const;
+    int funcaoHash(int chave);
+    void inserirItem(int chave, string valor);
+    void removerItem(int chave);
+    string pesquisarTabela(int chave);
+    void mostrarTabela();    
+};
+
+bool HashTable::estaVazia() const
+    {
+        int soma{};
+        for (int i{}; i < hashGroups; i++)
+        {
+            soma += tabela[i].size();
+        }
+        if(!soma){
+            return true;
+        }
+        return false;
+    }
+
+    int HashTable::funcaoHash(int chave){
+        return chave % hashGroups; //id
+    }
+
+    void HashTable::inserirItem(int chave, string valor){
+        int valorHash = funcaoHash(chave);
+        auto& celula = tabela[valorHash];
+        auto bItr = begin(celula);
+        bool existChave = false;
+        for(; bItr !=  end(celula); bItr++){
+            if(bItr->first == chave){
+                existChave = true;
+                bItr->second = valor;
+                cout<<"Atenção, valor existente. Valor substituido"<<endl;
+                break;
+            }
+        }
+        if(!existChave){
+            celula.emplace_back(chave,valor);
+        }
+        return;
+    }
+
+    void HashTable::removerItem(int chave){
+        int valorHash = funcaoHash(chave);
+        auto& celula = tabela[valorHash];
+        auto bItr = begin(celula);
+        bool existChave = false;
+        for(; bItr !=  end(celula); bItr++){
+            if(bItr->first == chave){
+                existChave = true;
+                bItr= celula.erase(bItr);
+                cout<<"Atenção, Item removido"<<endl;
+                break;
+            }
+        }
+        if(!existChave){
+            cout<<"Chave não encontrada"<<endl;
+        }
+    }
+    void HashTable::mostrarTabela(){
+        for(int i{}; i < hashGroups; i++){
+            if(tabela[i].size() == 0) continue;
+
+            auto bItr = tabela[i].begin();
+            for(;bItr != tabela[i].end(); bItr++){
+                cout<<"Chave: "<<bItr->first<<" Valor: "<<bItr->second<<endl;
+            }
+            
+        }
+    }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
+
 #include <iostream>
-#include <stdio.h>
-
-
+#include <vector>
+#include <string>
+#include <list>
+#include "HashTable.h"
+//#include "leitura.h"
 using namespace std;
 
-int main() {
- 
+int main(){
+
+    list<string> lista = {"int","main","(",")","{","int",
+    "n","=","2",";","n","+","=","1",";","}","\"Este é literal\"",
+    "}",">","+","}","+","+"
+    };
+    /*
+    Leitor l;
+    list<string> lista1 = l.ler("exemplo2.c");
+    cout << "SAIDA:" << endl;
+    for (string s : lista1)
+    {
+        cout << "<" << s << "> ";
+    }*/
+    HashTable HT;
+    int tamanhoLista = lista.size();
+    cout<<"Tamanho da lista: "<<tamanhoLista<<endl;
+    
+    for(int i{}; i < tamanhoLista; i++){
+        string s = lista.front();
+        
+        lista.pop_front();
+        
+        HT.inserirItem(i+1,s);
+    }
+    cout<<"Tabela de Simbolos\n"<<endl;
+
+    HT.mostrarTabela();        
 
 }
+
